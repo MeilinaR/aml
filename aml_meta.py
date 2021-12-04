665ca23d2fe6202bd8edf5ed68d88bafb9cd97e6
WesselvanDam <wesselvandam@hotmail.com>
2021-12-04 17:06:57 +0100

    minor edit

--- aml_meta.py
+++ aml_meta.py
@@ -4,6 +4,6 @@ class AMLOptimizer(MetaOptimizer):
   """Modified Learning to learn (meta) optimizer."""
   
   def __init__(self, **kwargs):
-    super.__init__(self, **kwargs)
+    super().__init__(**kwargs)
 
   # TODO: override the functions to match our proposed update rule
